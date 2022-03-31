1350fed2f2548972d0136f8ef303738870b8a098
dopaemon <polarisdp@gmail.com>
2022-03-31 10:45:00 +0000

    CI: Update Scripts Upload
    Signed-off-by: dopaemon <polarisdp@gmail.com>

---
+++ github-release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+repo=dopaemon/x-ui
+
+upload_url=$(curl -s -H "Authorization: token $token"  \
+     -d '{"tag_name": "test", "name":"release-0.0.1","body":"this is a test release"}'  \
+     "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url')
+
+upload_url="${upload_url%\{*}"
+
+echo "uploading asset to release to url : $upload_url"
+
+curl -s -H "Authorization: token $token"  \
+        -H "Content-Type: application/zip" \
+        --data-binary @/tmp/x-ui/src-zip/x-ui-linux-amd64.tar.gz  \
+        "$upload_url?name=x-ui-linux-amd64.tar.gz&label=x-ui-linux-amd64.tar.gz"
