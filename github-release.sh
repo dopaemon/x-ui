fccd81141d8a5d9653febb25ba1aafbc5c8e69cf
dopaemon <polarisdp@gmail.com>
2022-03-31 12:24:13 +0000

    CI: Relocal Tar.gz
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- github-release.sh
+++ github-release.sh
@@ -10,12 +10,16 @@ upload_url="${upload_url%\{*}"
 
 echo "uploading asset to release to url : $upload_url"
 
-curl -s -H "Authorization: token $token"  \
-        -H "Content-Type: application/gzip" \
-        --data-binary @/tmp/x-ui/x-ui-linux-amd64.tar.gz  \
-        "$upload_url?name=x-ui-linux-amd64.tar.gz&label=x-ui-linux-amd64.tar.gz"
+echo "Uploading amd64 Compile OUTPUT"
 
 curl -s -H "Authorization: token $token"  \
         -H "Content-Type: application/gzip" \
-        --data-binary @/tmp/x-ui/x-ui-linux-arm64.tar.gz  \
+        --data-binary @/tmp/x-ui/src-zip/x-ui-linux-amd64.tar.gz  \
+        "$upload_url?name=x-ui-linux-amd64.tar.gz&label=x-ui-linux-amd64.tar.gz"
+
+echo "Uploading arm64 Compile OUTPUT"
+
+curl -s -H "Authorization: token $token"  \
+        -H "Content-Type: application/gzip" \
+        --data-binary @/tmp/x-ui/src-zip/x-ui-linux-arm64.tar.gz  \
         "$upload_url?name=x-ui-linux-arm64.tar.gz&label=x-ui-linux-arm64.tar.gz"
