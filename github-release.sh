851539492d99f7c9eaa612af5a79e298a711c075
dopaemon <polarisdp@gmail.com>
2022-03-31 11:38:43 +0000

    CI: Create Zip arm64/amd64 Vietnamese Language
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- github-release.sh
+++ github-release.sh
@@ -3,7 +3,7 @@
 repo=dopaemon/x-ui
 
 upload_url=$(curl -s -H "Authorization: token $token"  \
-     -d '{"tag_name": "test", "name":"release-0.0.1","body":"this is a test release"}'  \
+     -d '{"tag_name": "0.3.2", "name":"X-UI 0.3.2 Việt Hoá","body":"Việt Hoá X-UI Từ Bản Gốc"}'  \
      "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url')
 
 upload_url="${upload_url%\{*}"
@@ -12,5 +12,10 @@ echo "uploading asset to release to url : $upload_url"
 
 curl -s -H "Authorization: token $token"  \
         -H "Content-Type: application/gzip" \
-        --data-binary @/tmp/x-ui/src-zip/x-ui-linux-amd64.tar.gz  \
+        --data-binary @/tmp/x-ui/x-ui-linux-amd64.tar.gz  \
         "$upload_url?name=x-ui-linux-amd64.tar.gz&label=x-ui-linux-amd64.tar.gz"
+
+curl -s -H "Authorization: token $token"  \
+        -H "Content-Type: application/gzip" \
+        --data-binary @/tmp/x-ui/x-ui-linux-arm64.tar.gz  \
+        "$upload_url?name=x-ui-linux-arm64.tar.gz&label=x-ui-linux-arm64.tar.gz"
