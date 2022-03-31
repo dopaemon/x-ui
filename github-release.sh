117a7fb3c72f229852e607389f0ac2cbadbddbaa
dopaemon <polarisdp@gmail.com>
2022-03-31 11:14:36 +0000

    CI: Added GZip
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- github-release.sh
+++ github-release.sh
@@ -11,6 +11,6 @@ upload_url="${upload_url%\{*}"
 echo "uploading asset to release to url : $upload_url"
 
 curl -s -H "Authorization: token $token"  \
-        -H "Content-Type: application/zip" \
+        -H "Content-Type: application/gzip" \
         --data-binary @/tmp/x-ui/src-zip/x-ui-linux-amd64.tar.gz  \
         "$upload_url?name=x-ui-linux-amd64.tar.gz&label=x-ui-linux-amd64.tar.gz"
