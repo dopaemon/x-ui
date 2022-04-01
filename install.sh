ec3b244219a306021b33f1a4ed7dc4cf77d0b23c
dopaemon <polarisdp@gmail.com>
2022-04-01 04:33:57 +0000

    language: Translate Script Vietnamese Language
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ plain='\033[0m'
 cur_dir=$(pwd)
 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${red}Lỗi：${plain} Bạn cần chạy tập lệnh này dưới user root ( không phải sudo ) ！\n" && exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -26,7 +26,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+    echo -e "${red}Phiên bản Linux bạn đang dùng không thể xác định！${plain}\n" && exit 1
 fi
 
 arch=$(arch)
@@ -37,13 +37,13 @@ elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
     arch="arm64"
 else
     arch="amd64"
-    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+    echo -e "${red}Không thể xác định cấu trúc CPU, thử dùng Kiến Trúc amd64 ${arch}${plain}"
 fi
 
-echo "架构: ${arch}"
+echo "Kiến Trúc CPU: ${arch}"
 
 if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
-    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
+    echo "Không hỗ trợ các hệ điều hành 32Bit vui lòng sử dụng hệ điều hành 64Bit ( Hoặc đợi cập nhật trong tương lai )"
     exit -1
 fi
 
@@ -59,15 +59,15 @@ fi
 
 if [[ x"${release}" == x"centos" ]]; then
     if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Bạn dùng dùng phiên bản CentOS 7 hoặc mới hơn！${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Bạn dùng dùng phiên bản Ubuntu 16.04 hoặc mới hơn！${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"debian" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Bạn dùng dùng phiên bản Debian 8 hoặc mới hơn！${plain}\n" && exit 1
     fi
 fi
 
@@ -81,22 +81,22 @@ install_base() {
 
 #This function will be called when user installed x-ui out of sercurity
 config_after_install() {
-    echo -e "${yellow}出于安全考虑，安装完成后需要强制修改端口与账户密码${plain}"
-    read -p "请设置您的账户名:" config_account
-    echo -e "${yellow}您的账户名将设定为:${config_account}${plain}"
-    read -p "请设置您的账户密码:" config_password
-    echo -e "${yellow}您的账户密码将设定为:${config_password}${plain}"
-    read -p "请设置面板访问端口:" config_port
-    echo -e "${yellow}您的面板访问端口将设定为:${config_port}${plain}"
-    read -p "确认设定完成？[y/n]": config_confirm
+    echo -e "${yellow}Vì lí do bảo mật, bạn nên đổi mật khẩu và tài khoản sau khi thiết lập xong ( hoặc cả port nếu bạn muốn )${plain}"
+    read -p "Tài khoản: " config_account
+    echo -e "${yellow}Tên tài khoản bạn đặt là: ${config_account}${plain}"
+    read -p "Mật khẩu:" config_password
+    echo -e "${yellow}Mật khẩu của bạn đặt là: ${config_password}${plain}"
+    read -p "Nhập Port:" config_port
+    echo -e "${yellow}Port bạn sử dụng là: ${config_port}${plain}"
+    read -p "Xác nhận cài đặt hoàn tất ？[y/n]": config_confirm
     if [[ x"${config_confirm}" == x"y" || x"${config_confirm}" == x"Y" ]]; then
-        echo -e "${yellow}确认设定,设定中${plain}"
+        echo -e "${yellow}Xác nhận cài đặt${plain}"
         /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
-        echo -e "${yellow}账户密码设定完成${plain}"
+        echo -e "${yellow}Hoàn tất đặt mật khẩu${plain}"
         /usr/local/x-ui/x-ui setting -port ${config_port}
-        echo -e "${yellow}面板端口设定完成${plain}"
+        echo -e "${yellow}Hoàn tất đặt Port${plain}"
     else
-        echo -e "${red}已取消,所有设置项均为默认设置,请及时修改${plain}"
+        echo -e "${red}Đã hủy cài đặt: Vui lòng cài đặt X-UI lại hoặc gỡ cài đặt mặc định, lý do bảo mật.${plain}"
     fi
 }
 
@@ -107,22 +107,22 @@ install_x-ui() {
     if [ $# == 0 ]; then
         last_version=$(curl -Ls "https://api.github.com/repos/dopaemon/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}检测 x-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 x-ui 版本安装${plain}"
+            echo -e "${red}Không thể xác định phiên bản X-UI, vui lòng kiểm tra github release hoặc API Github đã bị đổi${plain}"
             exit 1
         fi
-        echo -e "检测到 x-ui 最新版本：${last_version}，开始安装"
+        echo -e "Đã xác định bản Latest của X-UI：${last_version}，Bắt đầu cài đặt"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/dopaemon/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
+            echo -e "${red}Không thể tải xuống X-UI, kiểm tra bộ nhớ, hoặc Internet${plain}"
             exit 1
         fi
     else
         last_version=$1
         url="https://github.com/dopaemon/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
-        echo -e "开始安装 x-ui v$1"
+        echo -e "Bắt đầu cài đặt x-ui v$1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui v$1 失败，请确保此版本存在${plain}"
+            echo -e "${red}Không thể tải x-ui v$1，Đảm bảo rằng phiên bản này tồn tại${plain}"
             exit 1
         fi
     fi
@@ -140,34 +140,28 @@ install_x-ui() {
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
-    #echo -e "如果是全新安装，默认网页端口为 ${green}54321${plain}，用户名和密码默认都是 ${green}admin${plain}"
-    #echo -e "请自行确保此端口没有被其他程序占用，${yellow}并且确保 54321 端口已放行${plain}"
-    #    echo -e "若想将 54321 修改为其它端口，输入 x-ui 命令进行修改，同样也要确保你修改的端口也是放行的"
-    #echo -e ""
-    #echo -e "如果是更新面板，则按你之前的方式访问面板"
-    #echo -e ""
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
+    echo -e "${green}x-ui v${last_version}${plain} Quá trình cài đặt hoàn tất, bảng điều khiển được khởi chạy，"
     echo -e ""
-    echo -e "x-ui 管理脚本使用方法: "
+    echo -e "x-ui Cách sử dụng tập lệnh quản lý: "
     echo -e "----------------------------------------------"
-    echo -e "x-ui              - 显示管理菜单 (功能更多)"
-    echo -e "x-ui start        - 启动 x-ui 面板"
-    echo -e "x-ui stop         - 停止 x-ui 面板"
-    echo -e "x-ui restart      - 重启 x-ui 面板"
-    echo -e "x-ui status       - 查看 x-ui 状态"
-    echo -e "x-ui enable       - 设置 x-ui 开机自启"
-    echo -e "x-ui disable      - 取消 x-ui 开机自启"
-    echo -e "x-ui log          - 查看 x-ui 日志"
-    echo -e "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
-    echo -e "x-ui update       - 更新 x-ui 面板"
-    echo -e "x-ui install      - 安装 x-ui 面板"
-    echo -e "x-ui uninstall    - 卸载 x-ui 面板"
+    echo -e "x-ui              - Hiển thị menu quản lý (nhiều chức năng hơn)"
+    echo -e "x-ui start        - Khởi động bảng điều khiển x-ui"
+    echo -e "x-ui stop         - dừng bảng điều khiển x-ui"
+    echo -e "x-ui restart      - khởi động lại bảng điều khiển x-ui"
+    echo -e "x-ui status       - Xem trạng thái x-ui"
+    echo -e "x-ui enable       - Đặt x-ui để bắt đầu tự động khi khởi động"
+    echo -e "x-ui disable      - Hủy tự động khởi động x-ui boot"
+    echo -e "x-ui log          - Xem nhật ký x-ui"
+    echo -e "x-ui v2-ui        - Di chuyển dữ liệu tài khoản v2-ui của máy này sang x-ui"
+    echo -e "x-ui update       - Cập nhật bảng điều khiển x-ui"
+    echo -e "x-ui install      - cài đặt bảng điều khiển x-ui"
+    echo -e "x-ui uninstall    - Gỡ cài đặt bảng điều khiển x-ui"
     echo -e "----------------------------------------------"
 }
 
-echo -e "${green}开始安装${plain}"
+echo -e "${green}bắt đầu cài đặt${plain}"
 install_base
 install_x-ui $1
--- x-ui.sh
+++ x-ui.sh
@@ -18,7 +18,7 @@ function LOGI() {
     echo -e "${green}[INF] $* ${plain}"
 }
 # check root
-[[ $EUID -ne 0 ]] && LOGE "错误:  必须使用root用户运行此脚本！\n" && exit 1
+[[ $EUID -ne 0 ]] && LOGE "Lỗi: Bạn cần chạy Script này dưới quyền root ( Không phải sudo ) ！\n" && exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -36,7 +36,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    LOGE "未检测到系统版本，请联系脚本作者！\n" && exit 1
+    LOGE "Phiên bản Linux không thể xác định, liên hệ dev！\n" && exit 1
 fi
 
 os_version=""
@@ -51,21 +51,21 @@ fi
 
 if [[ x"${release}" == x"centos" ]]; then
     if [[ ${os_version} -le 6 ]]; then
-        LOGE "请使用 CentOS 7 或更高版本的系统！\n" && exit 1
+        LOGE "Bạn cần dùng thấp nhất bản CentOS 7 ！\n" && exit 1
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
-        LOGE "请使用 Ubuntu 16 或更高版本的系统！\n" && exit 1
+        LOGE "Bạn cần dùng thấp nhất bản Ubuntu 16.04 ！\n" && exit 1
     fi
 elif [[ x"${release}" == x"debian" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        LOGE "请使用 Debian 8 或更高版本的系统！\n" && exit 1
+        LOGE "Bạn cần dùng thấp nhất bản Debian 8 ！\n" && exit 1
     fi
 fi
 
 confirm() {
     if [[ $# > 1 ]]; then
-        echo && read -p "$1 [默认$2]: " temp
+        echo && read -p "$1 [Mặc định$2]: " temp
         if [[ x"${temp}" == x"" ]]; then
             temp=$2
         fi
@@ -80,7 +80,7 @@ confirm() {
 }
 
 confirm_restart() {
-    confirm "是否重启面板，重启面板也会重启 xray" "y"
+    confirm "Có khởi động lại bảng điều khiển hay không, khởi động lại bảng điều khiển cũng sẽ khởi động lại xray" "y"
     if [[ $? == 0 ]]; then
         restart
     else
@@ -89,7 +89,7 @@ confirm_restart() {
 }
 
 before_show_menu() {
-    echo && echo -n -e "${yellow}按回车返回主菜单: ${plain}" && read temp
+    echo && echo -n -e "${yellow}Nhấn enter để quay lại menu chính: ${plain}" && read temp
     show_menu
 }
 
@@ -105,9 +105,9 @@ install() {
 }
 
 update() {
-    confirm "本功能会强制重装当前最新版，数据不会丢失，是否继续?" "n"
+    confirm "Chức năng này sẽ buộc cài đặt lại phiên bản mới nhất hiện tại, dữ liệu sẽ không bị mất, có tiếp tục không?" "n"
     if [[ $? != 0 ]]; then
-        LOGE "已取消"
+        LOGE "Đã hủy"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
@@ -115,13 +115,13 @@ update() {
     fi
     bash <(curl -Ls https://raw.githubusercontent.com/dopaemon/x-ui/master/install.sh)
     if [[ $? == 0 ]]; then
-        LOGI "更新完成，已自动重启面板 "
+        LOGI "Cập nhật hoàn tất, bảng điều khiển đã được tự động khởi động lại "
         exit 0
     fi
 }
 
 uninstall() {
-    confirm "确定要卸载面板吗，xray 也会卸载?" "n"
+    confirm "Bạn có chắc chắn muốn gỡ cài đặt bảng điều khiển không, xray cũng sẽ gỡ cài đặt ?" "n"
     if [[ $? != 0 ]]; then
         if [[ $# == 0 ]]; then
             show_menu
@@ -137,7 +137,7 @@ uninstall() {
     rm /usr/local/x-ui/ -rf
 
     echo ""
-    echo -e "卸载成功，如果你想删除此脚本，则退出脚本后运行 ${green}rm /usr/bin/x-ui -f${plain} 进行删除"
+    echo -e "Gỡ cài đặt thành công, nếu bạn muốn xóa Script này, hãy nhập lệnh ${green}rm /usr/bin/x-ui -f${plain} sau khi thoát Script để hoàn tất gỡ X-UI"
     echo ""
 
     if [[ $# == 0 ]]; then
@@ -146,7 +146,7 @@ uninstall() {
 }
 
 reset_user() {
-    confirm "确定要将用户名和密码重置为 admin 吗" "n"
+    confirm "Bạn có chắc chắn muốn đặt lại tên người dùng và mật khẩu cho admin không" "n"
     if [[ $? != 0 ]]; then
         if [[ $# == 0 ]]; then
             show_menu
@@ -154,12 +154,12 @@ reset_user() {
         return 0
     fi
     /usr/local/x-ui/x-ui setting -username admin -password admin
-    echo -e "用户名和密码已重置为 ${green}admin${plain}，现在请重启面板"
+    echo -e "Tên người dùng và mật khẩu đã được đặt lại thành ${green}admin${plain}，Vui lòng khởi động lại bảng điều khiển ngay bây giờ"
     confirm_restart
 }
 
 reset_config() {
-    confirm "确定要重置所有面板设置吗，账号数据不会丢失，用户名和密码不会改变" "n"
+    confirm "Bạn có chắc chắn muốn đặt lại tất cả cài đặt bảng điều khiển, dữ liệu tài khoản sẽ không bị mất, tên người dùng và mật khẩu sẽ không bị thay đổi" "n"
     if [[ $? != 0 ]]; then
         if [[ $# == 0 ]]; then
             show_menu
@@ -167,18 +167,18 @@ reset_config() {
         return 0
     fi
     /usr/local/x-ui/x-ui setting -reset
-    echo -e "所有面板设置已重置为默认值，现在请重启面板，并使用默认的 ${green}54321${plain} 端口访问面板"
+    echo -e "Tất cả cài đặt bảng điều khiển đã được đặt lại về mặc định, vui lòng khởi động lại bảng điều khiển ngay bây giờ và sử dụng cài đặt mặc định ${green}54321${plain} Bảng điều khiển truy cập cổng"
     confirm_restart
 }
 
 set_port() {
-    echo && echo -n -e "输入端口号[1-65535]: " && read port
+    echo && echo -n -e "Nhập Port [1-65535]: " && read port
     if [[ -z "${port}" ]]; then
-        LOGD "已取消"
+        LOGD "Đã hủy"
         before_show_menu
     else
         /usr/local/x-ui/x-ui setting -port ${port}
-        echo -e "设置端口完毕，现在请重启面板，并使用新设置的端口 ${green}${port}${plain} 访问面板"
+        echo -e "Sau khi thiết lập cổng, vui lòng khởi động lại bảng điều khiển và sử dụng Port mới đặt ${green}${port}${plain} bảng điều khiển truy cập"
         confirm_restart
     fi
 }
@@ -187,15 +187,15 @@ start() {
     check_status
     if [[ $? == 0 ]]; then
         echo ""
-        LOGI "面板已运行，无需再次启动，如需重启请选择重启"
+        LOGI "Bảng đã chạy rồi, không cần khởi động lại, nếu muốn khởi động lại, vui lòng chọn khởi động lại"
     else
         systemctl start x-ui
         sleep 2
         check_status
         if [[ $? == 0 ]]; then
-            LOGI "x-ui 启动成功"
+            LOGI "x-ui Đã bắt đầu thành công"
         else
-            LOGE "面板启动失败，可能是因为启动时间超过了两秒，请稍后查看日志信息"
+            LOGE "Bảng điều khiển không khởi động được, có thể do thời gian khởi động vượt quá hai giây, vui lòng kiểm tra thông tin nhật ký"
         fi
     fi
 
@@ -208,15 +208,15 @@ stop() {
     check_status
     if [[ $? == 1 ]]; then
         echo ""
-        LOGI "面板已停止，无需再次停止"
+        LOGI "Bảng điều khiển đã dừng, không cần dừng lại"
     else
         systemctl stop x-ui
         sleep 2
         check_status
         if [[ $? == 1 ]]; then
-            LOGI "x-ui 与 xray 停止成功"
+            LOGI "x-ui và xray dừng thành công"
         else
-            LOGE "面板停止失败，可能是因为停止时间超过了两秒，请稍后查看日志信息"
+            LOGE "Bảng điều khiển không dừng được, có thể do thời gian dừng vượt quá hai giây, vui lòng kiểm tra thông tin nhật ký sau"
         fi
     fi
 
@@ -230,9 +230,9 @@ restart() {
     sleep 2
     check_status
     if [[ $? == 0 ]]; then
-        LOGI "x-ui 与 xray 重启成功"
+        LOGI "x-ui và xray đã khởi động lại thành công"
     else
-        LOGE "面板重启失败，可能是因为启动时间超过了两秒，请稍后查看日志信息"
+        LOGE "Bảng điều khiển không thể khởi động lại, có thể do thời gian khởi động vượt quá hai giây, vui lòng kiểm tra thông tin nhật ký sau"
     fi
     if [[ $# == 0 ]]; then
         before_show_menu
@@ -249,9 +249,9 @@ status() {
 enable() {
     systemctl enable x-ui
     if [[ $? == 0 ]]; then
-        LOGI "x-ui 设置开机自启成功"
+        LOGI "x-ui đặt khởi động thành công"
     else
-        LOGE "x-ui 设置开机自启失败"
+        LOGE "x-ui không đặt được tự động khởi động khi khởi động"
     fi
 
     if [[ $# == 0 ]]; then
@@ -262,9 +262,9 @@ enable() {
 disable() {
     systemctl disable x-ui
     if [[ $? == 0 ]]; then
-        LOGI "x-ui 取消开机自启成功"
+        LOGI "x-ui hủy khởi động tự khởi động thành công"
     else
-        LOGE "x-ui 取消开机自启失败"
+        LOGE "x-ui không thể hủy chế độ tự khởi động"
     fi
 
     if [[ $# == 0 ]]; then
@@ -296,11 +296,11 @@ update_shell() {
     wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/dopaemon/x-ui/raw/master/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
-        LOGE "下载脚本失败，请检查本机能否连接 Github"
+        LOGE "Tập lệnh tải xuống không thành công, vui lòng kiểm tra xem máy có thể kết nối được không Github"
         before_show_menu
     else
         chmod +x /usr/bin/x-ui
-        LOGI "升级脚本成功，请重新运行脚本" && exit 0
+        LOGI "Tập lệnh nâng cấp thành công, vui lòng chạy lại tập lệnh" && exit 0
     fi
 }
 
@@ -330,7 +330,7 @@ check_uninstall() {
     check_status
     if [[ $? != 2 ]]; then
         echo ""
-        LOGE "面板已安装，请不要重复安装"
+        LOGE "Bảng điều khiển đã được cài đặt, vui lòng không cài đặt lại"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
@@ -344,7 +344,7 @@ check_install() {
     check_status
     if [[ $? == 2 ]]; then
         echo ""
-        LOGE "请先安装面板"
+        LOGE "Vui lòng cài đặt bảng điều khiển trước"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
@@ -358,15 +358,15 @@ show_status() {
     check_status
     case $? in
     0)
-        echo -e "面板状态: ${green}已运行${plain}"
+        echo -e "tình trạng bảng điều khiển: ${green}đã được chạy${plain}"
         show_enable_status
         ;;
     1)
-        echo -e "面板状态: ${yellow}未运行${plain}"
+        echo -e "tình trạng bảng điều khiển: ${yellow}không chạy${plain}"
         show_enable_status
         ;;
     2)
-        echo -e "面板状态: ${red}未安装${plain}"
+        echo -e "tình trạng bảng điều khiển: ${red}Chưa cài đặt${plain}"
         ;;
     esac
     show_xray_status
@@ -375,9 +375,9 @@ show_status() {
 show_enable_status() {
     check_enabled
     if [[ $? == 0 ]]; then
-        echo -e "是否开机自启: ${green}是${plain}"
+        echo -e "Tự động bắt đầu không: ${green}Có${plain}"
     else
-        echo -e "是否开机自启: ${red}否${plain}"
+        echo -e "Tự động bắt đầu không: ${red}Không${plain}"
     fi
 }
 
@@ -393,27 +393,27 @@ check_xray_status() {
 show_xray_status() {
     check_xray_status
     if [[ $? == 0 ]]; then
-        echo -e "xray 状态: ${green}运行${plain}"
+        echo -e "Khởi chạy Xray: ${green}Khởi Chạy${plain}"
     else
-        echo -e "xray 状态: ${red}未运行${plain}"
+        echo -e "Khởi chạy Xray: ${red}Không khởi chạy${plain}"
     fi
 }
 
 ssl_cert_issue() {
     echo -E ""
-    LOGD "******使用说明******"
-    LOGI "该脚本将使用Acme脚本申请证书,使用时需保证:"
-    LOGI "1.知晓Cloudflare 注册邮箱"
-    LOGI "2.知晓Cloudflare Global API Key"
-    LOGI "3.域名已通过Cloudflare进行解析到当前服务器"
-    LOGI "4.该脚本申请证书默认安装路径为/root/cert目录"
-    confirm "我已确认以上内容[y/n]" "y"
+    LOGD "******Hướng dẫn sử dụng******"
+    LOGI "Tập lệnh này sẽ sử dụng tập lệnh Acme để đăng ký chứng chỉ và bạn cần đảm bảo rằng nó được sử dụng:"
+    LOGI "1.Biết email đã đăng ký Cloudflare"
+    LOGI "2.Biết Cloudflare Global API Key"
+    LOGI "3.Tên miền đã được giải quyết cho máy chủ hiện tại thông qua Cloudflare"
+    LOGI "4.Đường dẫn cài đặt mặc định cho tập lệnh này để áp dụng cho chứng chỉ là /root/cert Nội dung"
+    confirm "Tôi đã xác nhận ở trên [y/n]" "y"
     if [ $? -eq 0 ]; then
         cd ~
-        LOGI "安装Acme脚本"
+        LOGI "Cài đặt Acme Script"
         curl https://get.acme.sh | sh
         if [ $? -ne 0 ]; then
-            LOGE "安装acme脚本失败"
+            LOGE "Không cài đặt được Script acme"
             exit 1
         fi
         CF_Domain=""
@@ -426,46 +426,46 @@ ssl_cert_issue() {
             rm -rf $certPath
             mkdir $certPath
         fi
-        LOGD "请设置域名:"
-        read -p "Input your domain here:" CF_Domain
-        LOGD "你的域名设置为:${CF_Domain}"
-        LOGD "请设置API密钥:"
-        read -p "Input your key here:" CF_GlobalKey
-        LOGD "你的API密钥为:${CF_GlobalKey}"
-        LOGD "请设置注册邮箱:"
-        read -p "Input your email here:" CF_AccountEmail
-        LOGD "你的注册邮箱为:${CF_AccountEmail}"
+        LOGD "Vui lòng đặt tên miền: "
+        read -p "Nhập domain của bạn ở đây:" CF_Domain
+        LOGD "Domain của bạn được đặt thành:${CF_Domain}"
+        LOGD "Nhập API: "
+        read -p "Nhập API key ở đây: " CF_GlobalKey
+        LOGD "Khóa API của bạn là: ${CF_GlobalKey}"
+        LOGD "Vui lòng đặt địa chỉ email đã đăng ký: "
+        read -p "Nhập Email của bạn: " CF_AccountEmail
+        LOGD "Email đã đăng ký của bạn là: ${CF_AccountEmail}"
         ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
         if [ $? -ne 0 ]; then
-            LOGE "修改默认CA为Lets'Encrypt失败,脚本退出"
+            LOGE "Sửa đổi CA mặc định thành Lets'Encrypt không thành công và tập lệnh thoát"
             exit 1
         fi
         export CF_Key="${CF_GlobalKey}"
         export CF_Email=${CF_AccountEmail}
         ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain} -d *.${CF_Domain} --log
         if [ $? -ne 0 ]; then
-            LOGE "证书签发失败,脚本退出"
+            LOGE "Cấp chứng chỉ không thành công, tập lệnh thoát"
             exit 1
         else
-            LOGI "证书签发成功,安装中..."
+            LOGI "Chứng chỉ được cấp thành công, quá trình cài đặt đang diễn ra..."
         fi
         ~/.acme.sh/acme.sh --installcert -d ${CF_Domain} -d *.${CF_Domain} --ca-file /root/cert/ca.cer \
             --cert-file /root/cert/${CF_Domain}.cer --key-file /root/cert/${CF_Domain}.key \
             --fullchain-file /root/cert/fullchain.cer
         if [ $? -ne 0 ]; then
-            LOGE "证书安装失败,脚本退出"
+            LOGE "Cài đặt chứng chỉ không thành công, tập lệnh đã thoát"
             exit 1
         else
-            LOGI "证书安装成功,开启自动更新..."
+            LOGI "Chứng chỉ được cài đặt thành công, cho phép cập nhật tự động..."
         fi
         ~/.acme.sh/acme.sh --upgrade --auto-upgrade
         if [ $? -ne 0 ]; then
-            LOGE "自动更新设置失败,脚本退出"
+            LOGE "Cài đặt cập nhật tự động không thành công, tập lệnh đã thoát"
             ls -lah cert
             chmod 755 $certPath
             exit 1
         else
-            LOGI "证书已安装且已开启自动更新,具体信息如下"
+            LOGI "Chứng chỉ đã được cài đặt và cập nhật tự động đã được bật, thông tin chi tiết như sau"
             ls -lah cert
             chmod 755 $certPath
         fi
@@ -475,50 +475,50 @@ ssl_cert_issue() {
 }
 
 show_usage() {
-    echo "x-ui 管理脚本使用方法: "
-    echo "------------------------------------------"
-    echo "x-ui              - 显示管理菜单 (功能更多)"
-    echo "x-ui start        - 启动 x-ui 面板"
-    echo "x-ui stop         - 停止 x-ui 面板"
-    echo "x-ui restart      - 重启 x-ui 面板"
-    echo "x-ui status       - 查看 x-ui 状态"
-    echo "x-ui enable       - 设置 x-ui 开机自启"
-    echo "x-ui disable      - 取消 x-ui 开机自启"
-    echo "x-ui log          - 查看 x-ui 日志"
-    echo "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
-    echo "x-ui update       - 更新 x-ui 面板"
-    echo "x-ui install      - 安装 x-ui 面板"
-    echo "x-ui uninstall    - 卸载 x-ui 面板"
-    echo "------------------------------------------"
+    echo -e "x-ui Cách sử dụng tập lệnh quản lý: "
+    echo -e "----------------------------------------------"
+    echo -e "x-ui              - Hiển thị menu quản lý (nhiều chức năng hơn)"
+    echo -e "x-ui start        - Khởi động bảng điều khiển x-ui"
+    echo -e "x-ui stop         - dừng bảng điều khiển x-ui"
+    echo -e "x-ui restart      - khởi động lại bảng điều khiển x-ui"
+    echo -e "x-ui status       - Xem trạng thái x-ui"
+    echo -e "x-ui enable       - Đặt x-ui để bắt đầu tự động khi khởi động"
+    echo -e "x-ui disable      - Hủy tự động khởi động x-ui boot"
+    echo -e "x-ui log          - Xem nhật ký x-ui"
+    echo -e "x-ui v2-ui        - Di chuyển dữ liệu tài khoản v2-ui của máy này sang x-ui"
+    echo -e "x-ui update       - Cập nhật bảng điều khiển x-ui"
+    echo -e "x-ui install      - cài đặt bảng điều khiển x-ui"
+    echo -e "x-ui uninstall    - Gỡ cài đặt bảng điều khiển x-ui"
+    echo -e "----------------------------------------------"
 }
 
 show_menu() {
     echo -e "
-  ${green}x-ui 面板管理脚本${plain}
-  ${green}0.${plain} 退出脚本
+  ${green}x-ui Tập lệnh quản lý bảng điều khiển${plain}
+  ${green}0.${plain} Thoát Script
 ————————————————
-  ${green}1.${plain} 安装 x-ui
-  ${green}2.${plain} 更新 x-ui
-  ${green}3.${plain} 卸载 x-ui
+  ${green}1.${plain} Cài đặt x-ui
+  ${green}2.${plain} Cập Nhật x-ui
+  ${green}3.${plain} Gỡ Cài Đặt x-ui
 ————————————————
-  ${green}4.${plain} 重置用户名密码
-  ${green}5.${plain} 重置面板设置
-  ${green}6.${plain} 设置面板端口
+  ${green}4.${plain} đặt lại mật khẩu tên người dùng
+  ${green}5.${plain} đặt lại cài đặt bảng điều khiển
+  ${green}6.${plain} Thiết lập các Port bảng điều khiển
 ————————————————
-  ${green}7.${plain} 启动 x-ui
-  ${green}8.${plain} 停止 x-ui
-  ${green}9.${plain} 重启 x-ui
- ${green}10.${plain} 查看 x-ui 状态
- ${green}11.${plain} 查看 x-ui 日志
+  ${green}7.${plain} khởi động x-ui
+  ${green}8.${plain} Ngừng x-ui
+  ${green}9.${plain} khởi động lại x-ui
+ ${green}10.${plain} Xem trạng thái x-ui
+ ${green}11.${plain} Xem nhật ký x-ui
 ————————————————
- ${green}12.${plain} 设置 x-ui 开机自启
- ${green}13.${plain} 取消 x-ui 开机自启
+ ${green}12.${plain} Đặt x-ui để bắt đầu tự động khi khởi động
+ ${green}13.${plain} Hủy tự động khởi động x-ui boot
 ————————————————
- ${green}14.${plain} 一键安装 bbr (最新内核)
- ${green}15.${plain} 一键申请SSL证书(acme申请)
+ ${green}14.${plain} cài đặt bbr (hạt nhân mới nhất)
+ ${green}15.${plain} Cài đặt chứng chỉ SSL (ứng dụng acme)
  "
     show_status
-    echo && read -p "请输入选择 [0-14]: " num
+    echo && read -p "Vui lòng nhập một lựa chọn [0-14]: " num
 
     case "${num}" in
     0)
@@ -570,7 +570,7 @@ show_menu() {
         ssl_cert_issue
         ;;
     *)
-        LOGE "请输入正确的数字 [0-14]"
+        LOGE "Vui lòng nhập lựa chọn [0-14]"
         ;;
     esac
 }
