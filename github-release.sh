abe2b3436fe18810887f8e8da95cf8754b496dc4
dopaemon <polarisdp@gmail.com>
2022-04-10 02:50:19 +0700

    CI: ReWrite Var Token
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- github-release.sh
+++ github-release.sh
@@ -2,18 +2,18 @@
 
 repo=dopaemon/x-ui
 
-upload_url=$(curl -s -H "Authorization: token $token"  \
+upload_url=$(curl -s -H "Authorization: token $TOKEN"  \
      -d '{"tag_name": "0.3.4", "name":"X-UI "$VERSIONS" Việt Hoá","body":"$INFORELEASE"}'  \
-     "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url') > /dev/null 2>&1
+     "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url')
 
 upload_url="${upload_url%\{*}" > /dev/null 2>&1
 
-curl -s -H "Authorization: token $token"  \
+curl -s -H "Authorization: token $TOKEN"  \
         -H "Content-Type: application/gzip" \
         --data-binary @/tmp/ci/amd64/x-ui-linux-amd64.tar.gz  \
-        "$upload_url?name=x-ui-linux-amd64.tar.gz&label=x-ui-linux-amd64.tar.gz" > /dev/null 2>&1
+        "$upload_url?name=x-ui-linux-amd64.tar.gz&label=x-ui-linux-amd64.tar.gz"
 
-curl -s -H "Authorization: token $token"  \
+curl -s -H "Authorization: token $TOKEN"  \
         -H "Content-Type: application/gzip" \
         --data-binary @/tmp/ci/arm64/x-ui-linux-arm64.tar.gz  \
-        "$upload_url?name=x-ui-linux-arm64.tar.gz&label=x-ui-linux-arm64.tar.gz" > /dev/null 2>&1
+        "$upload_url?name=x-ui-linux-arm64.tar.gz&label=x-ui-linux-arm64.tar.gz"
