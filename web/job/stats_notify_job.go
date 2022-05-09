9e35e06b75d80f785ae5698230ed2154f5a817e6
FranzKafkayu <franzkafkayu@gmail.com>
2022-05-10 00:03:02 +0800

    @FranzKafkayu: add tg bot command
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- web/job/stats_notify_job.go
+++ web/job/stats_notify_job.go
@@ -10,8 +10,6 @@ import (
 	"x-ui/logger"
 	"x-ui/util/common"
 	"x-ui/web/service"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 var SSHLoginUser int
@@ -24,42 +22,15 @@ const (
 )
 
 type StatsNotifyJob struct {
-	enable         bool
-	xrayService    service.XrayService
-	inboundService service.InboundService
-	settingService service.SettingService
+	enable          bool
+	telegramService service.TelegramService
+	xrayService     service.XrayService
+	inboundService  service.InboundService
+	settingService  service.SettingService
 }
 
 func NewStatsNotifyJob() *StatsNotifyJob {
 	return new(StatsNotifyJob)
-}
-
-func (j *StatsNotifyJob) SendMsgToTgbot(msg string) {
-	//Telegram bot basic info
-	tgBottoken, err := j.settingService.GetTgBotToken()
-	if err != nil {
-		logger.Warning("sendMsgToTgbot failed,GetTgBotToken fail:", err)
-		return
-	}
-	tgBotid, err := j.settingService.GetTgBotChatId()
-	if err != nil {
-		logger.Warning("sendMsgToTgbot failed,GetTgBotChatId fail:", err)
-		return
-	}
-	if tgBottoken == "" || tgBotid == 0 {
-		return
-	}
-
-	bot, err := tgbotapi.NewBotAPI(tgBottoken)
-	if err != nil {
-		fmt.Println("get tgbot error:", err)
-		return
-	}
-	bot.Debug = true
-	fmt.Printf("Authorized on account %s", bot.Self.UserName)
-	info := tgbotapi.NewMessage(int64(tgBotid), msg)
-	//msg.ReplyToMessageID = int(tgBotid)
-	bot.Send(info)
 }
 
 //Here run is a interface method of Job interface
@@ -68,58 +39,8 @@ func (j *StatsNotifyJob) Run() {
 		return
 	}
 	var info string
-	//get hostname
-	name, err := os.Hostname()
-	if err != nil {
-		fmt.Println("get hostname error:", err)
-		return
-	}
-	info = fmt.Sprintf("Tên máy chủ:%s\r\n", name)
-	//get ip address
-	var ip string
-	netInterfaces, err := net.Interfaces()
-	if err != nil {
-		fmt.Println("net.Interfaces failed, err:", err.Error())
-		return
-	}
-
-	for i := 0; i < len(netInterfaces); i++ {
-		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
-			addrs, _ := netInterfaces[i].Addrs()
-
-			for _, address := range addrs {
-				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-					if ipnet.IP.To4() != nil {
-						ip = ipnet.IP.String()
-						break
-					} else {
-						ip = ipnet.IP.String()
-						break
-					}
-                                }
-                        }
-                }
-        }
-
-	info += fmt.Sprintf("Địa Chỉ IP: %s\r\n \r\n", ip)
-
-	//get traffic
-	inbouds, err := j.inboundService.GetAllInbounds()
-	if err != nil {
-		logger.Warning("StatsNotifyJob run failed:", err)
-		return
-	}
-	//NOTE:If there no any sessions here,need to notify here
-	//TODO:分节点推送,自动转化格式
-	for _, inbound := range inbouds {
-		info += fmt.Sprintf("Tên Server: %s\r\nPort:%d\r\nLưu lượng tải lên ↑: %s\r\nLưu lượng tải xuống ↓: %s\r\nTổng lưu lượng truy cập: %s\r\n \r\n", inbound.Remark, inbound.Port, common.FormatTraffic(inbound.Up), common.FormatTraffic(inbound.Down), common.FormatTraffic((inbound.Up + inbound.Down)))
-		if inbound.ExpiryTime == 0 {
-			info += fmt.Sprintf("Hạn sử dụng: Vô thời hạn\r\n \r\n")
-		} else {
-			info += fmt.Sprintf("Hạn sử dụng: %s\r\n \r\n", time.Unix((inbound.ExpiryTime/1000), 0).Format("2006-01-02 15:04:05"))
-		}
-	}
-	j.SendMsgToTgbot(info)
+	info = j.GetsystemStatus()
+	j.telegramService.SendMsgToTgbot(info)
 }
 
 func (j *StatsNotifyJob) UserLoginNotify(username string, ip string, time string, status LoginStatus) {
@@ -143,7 +64,7 @@ func (j *StatsNotifyJob) UserLoginNotify(username string, ip string, time string
 	msg += fmt.Sprintf("Thời gian: %s\r\n", time)
 	msg += fmt.Sprintf("Tài khoản: %s\r\n", username)
 	msg += fmt.Sprintf("IP: %s\r\n", ip)
-	j.SendMsgToTgbot(msg)
+	j.telegramService.SendMsgToTgbot(msg)
 }
 
 func (j *StatsNotifyJob) SSHStatusLoginNotify(xuiStartTime string) {
@@ -200,14 +121,69 @@ func (j *StatsNotifyJob) SSHStatusLoginNotify(xuiStartTime string) {
 			return
 		}
 
-		SSHLoginInfo = fmt.Sprintf("Có phiên đăng nhập mới:\r\n")
+		SSHLoginInfo = fmt.Sprintf("Có phiên đăng nhập mới: \r\n")
 		SSHLoginInfo += fmt.Sprintf("Tên Server: %s\r\n", name)
 		SSHLoginInfo += fmt.Sprintf("SSH User: %s", SSHLoginUserName)
 		SSHLoginInfo += fmt.Sprintf("Thời gian đăng nhập: %s", SSHLoginTime)
 		SSHLoginInfo += fmt.Sprintf("IP Đăng nhập SSH: %s", SSHLoginIpAddr)
 		SSHLoginInfo += fmt.Sprintf("Số User đăng nhập SSH hiện tại: %s", getSSHUserNumber)
-		j.SendMsgToTgbot(SSHLoginInfo)
+		j.telegramService.SendMsgToTgbot(SSHLoginInfo)
 	} else {
 		SSHLoginUser = numberInt
 	}
+}
+
+func (j *StatsNotifyJob) GetsystemStatus() string {
+	var info string
+	//get hostname
+	name, err := os.Hostname()
+	if err != nil {
+		fmt.Println("get hostname error:", err)
+		return ""
+	}
+	info = fmt.Sprintf("Tên máy chủ: %s\r\n", name)
+	//get ip address
+	var ip string
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("net.Interfaces failed, err:", err.Error())
+		return ""
+	}
+
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						ip = ipnet.IP.String()
+						break
+					} else {
+						ip = ipnet.IP.String()
+						break
+					}
+				}
+			}
+		}
+	}
+	info += fmt.Sprintf("Địa chỉ IP: %s\r\n \r\n", ip)
+
+	//get traffic节点名
+	inbouds, err := j.inboundService.GetAllInbounds()
+	if err != nil {
+		logger.Warning("StatsNotifyJob run failed:", err)
+		return ""
+	}
+	//NOTE:If there no any sessions here,need to notify here
+	//TODO:分节点推送,自动转化格式
+	for _, inbound := range inbouds {
+		info += fmt.Sprintf("Tên Server VPN: :%s\r\nPort: %d\r\nData tải lên ↑: %s\r\nData tải xuống ↓: %s\r\nTổng Data: %s\r\n", inbound.Remark, inbound.Port, common.FormatTraffic(inbound.Up), common.FormatTraffic(inbound.Down), common.FormatTraffic((inbound.Up + inbound.Down)))
+		if inbound.ExpiryTime == 0 {
+			info += fmt.Sprintf("Data Tối Đa: Unlimited\r\n \r\n")
+		} else {
+			info += fmt.Sprintf("Data Tối Đa: %s\r\n \r\n", time.Unix((inbound.ExpiryTime/1000), 0).Format("2006-01-02 15:04:05"))
+		}
+	}
+	return info
 }
