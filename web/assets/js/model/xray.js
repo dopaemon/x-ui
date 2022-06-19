e7f077b61a133b41a95e096dfc40edb6bf004c78
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-06-19 10:18:53 +0800

    @misakano7545: Ss2022 (#17)
    * ss2022
    
    * ss2022
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- web/assets/js/model/xray.js
+++ web/assets/js/model/xray.js
@@ -24,9 +24,9 @@ const SSMethods = {
     CHACHA20_POLY1305: 'chacha20-poly1305',
     AES_256_GCM: 'aes-256-gcm',
     AES_128_GCM: 'aes-128-gcm',
-    BLAKE3_AES_128_GCM: '2022-blake3-aes-128-gcm',
-    BLAKE3_AES_256_GCM: '2022-blake3-aes-256-gcm',
-    BLAKE3_CHACHA20_POLY1305: '2022-blake3-chacha20-poly1305',
+    SS_2022_BLAKE3_AES_128_GCM: '2022-blake3-aes-128-gcm',
+    SS_2022_BLAKE3_AES_256_GCM: '2022-blake3-aes-256-gcm',
+    SS_2022_BLAKE3_CHACHA20_POLY1305: '2022-blake3-chacha20-poly1305',
 };
 
 const RULE_IP = {
