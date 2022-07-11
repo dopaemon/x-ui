7ce50259ff4758d1e1146aa76e950008433a18d1
awawa <108142172+awawa42@users.noreply.github.com>
2022-07-11 17:01:47 +0800

    @awawa42: Add vless muilti client configuration support on same port
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- web/assets/js/model/xray.js
+++ web/assets/js/model/xray.js
@@ -1265,6 +1265,14 @@ Inbound.VLESSSettings = class extends Inbound.Settings {
         this.fallbacks = fallbacks;
     }
 
+    addVLESS() {
+        this.vlesses.push(new Inbound.VLESSSettings.VLESS());
+    }
+
+    delVLESS(index) {
+        this.vlesses.splice(index, 1);
+    }
+    
     addFallback() {
         this.fallbacks.push(new Inbound.VLESSSettings.Fallback());
     }
