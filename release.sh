c6427a97c0e823097829cca8cd893d7b21d1995f
dopaemon <polarisdp@gmail.com>
2022-08-06 17:31:32 +0000

    Revert "AI: Fix"
    This reverts commit 1b71a2275c6afdd0ee450b420da608c1e274bb7b.

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-last_version=$(curl -Ls "https://api.github.com/repos/dopaemon/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-if [[ "$last_version" == "$VERSIONS" ]]; then
+gh release show $VERSIONS > /dev/null 2>&16	# > /dev/null 2>&16 is to hide the error message
+if [ $? -eq 0 ]; then
 	gh release delete $VERSIONS -y
 fi
